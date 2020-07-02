c695812ff08252e381fe27c8cb6fd8f2a09cabb3
Laura Valencia Gracia <59279392+kit57@users.noreply.github.com>
2020-07-02 13:00:14 +0200

    Add files

--- project/utils.py
+++ project/utils.py
@@ -2,6 +2,8 @@ import nltk
 import pickle
 import re
 import numpy as np
+import gensim
+from gensim.models import KeyedVectors
 
 nltk.download('stopwords')
 from nltk.corpus import stopwords
@@ -18,7 +20,7 @@ RESOURCE_PATH = {
 
 def text_prepare(text):
     """Performs tokenization and simple preprocessing."""
-
+    
     replace_by_space_re = re.compile('[/(){}\[\]\|@,;]')
     bad_symbols_re = re.compile('[^0-9a-z #+_]')
     stopwords_set = set(stopwords.words('english'))
@@ -33,15 +35,13 @@ def text_prepare(text):
 
 def load_embeddings(embeddings_path):
     """Loads pre-trained word embeddings from tsv file.
-
     Args:
       embeddings_path - path to the embeddings file.
-
     Returns:
       embeddings - dict mapping words to vectors;
       embeddings_dim - dimension of the vectors.
     """
-
+    
     # Hint: you have already implemented a similar routine in the 3rd assignment.
     # Note that here you also need to know the dimension of the loaded embeddings.
     # When you load the embeddings, use numpy.float32 type as dtype
@@ -51,26 +51,37 @@ def load_embeddings(embeddings_path):
     ########################
 
     # remove this when you're done
-    raise NotImplementedError(
-        "Open utils.py and fill with your code. In case of Google Colab, download"
-        "(https://github.com/hse-aml/natural-language-processing/blob/master/project/utils.py), "
-        "edit locally and upload using '> arrow on the left edge' -> Files -> UPLOAD")
-
+#     raise NotImplementedError(
+#         "Open utils.py and fill with your code. In case of Google Colab, download"
+#         "(https://github.com/hse-aml/natural-language-processing/blob/master/project/utils.py), "
+#         "edit locally and upload using '> arrow on the left edge' -> Files -> UPLOAD")
+    embeddings = gensim.models.KeyedVectors.load_word2vec_format(embeddings_path, binary=True,limit=500000)
+    embeddings_dim = 300
+    return embeddings, embeddings_dim
 
 def question_to_vec(question, embeddings, dim):
     """Transforms a string to an embedding by averaging word embeddings."""
-
+    
     # Hint: you have already implemented exactly this function in the 3rd assignment.
 
     ########################
     #### YOUR CODE HERE ####
     ########################
-
+    result = np.zeros(dim)
+    cnt = 0
+    words = question.split()
+    for word in words:
+        if word in embeddings:
+            result += np.array(embeddings[word])
+            cnt += 1
+    if cnt != 0:
+        result /= cnt
+    return result 
     # remove this when you're done
-    raise NotImplementedError(
-        "Open utils.py and fill with your code. In case of Google Colab, download"
-        "(https://github.com/hse-aml/natural-language-processing/blob/master/project/utils.py), "
-        "edit locally and upload using '> arrow on the left edge' -> Files -> UPLOAD")
+#     raise NotImplementedError(
+#         "Open utils.py and fill with your code. In case of Google Colab, download"
+#         "(https://github.com/hse-aml/natural-language-processing/blob/master/project/utils.py), "
+#         "edit locally and upload using '> arrow on the left edge' -> Files -> UPLOAD")
 
 
 def unpickle_file(filename):
